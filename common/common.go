e894f7216af60aecbf2076987abc4758d36e4c06
Konstantinos Lypitkas <klipitkas@gmail.com>
2019-10-28 11:57:31 +0200

    Include supplementary user groups on exec

--- common/common.go
+++ common/common.go
@@ -15,18 +15,23 @@ import (
 
 // ExecuteCommand runs a specific shell command in the target system.
 func ExecuteCommand(cmd string, username string, workDir string, args ...string) (string, error) {
-
 	command := exec.Command(cmd, args...)
 
 	var outBuf, errorBuf bytes.Buffer
-
 	if username != "" {
 		credentials, err := UserCredentialsFromUsername(username)
 		if err != nil {
-			return "", fmt.Errorf("get user id, gid from username %s: %v", username, err)
+			return "", fmt.Errorf("get user id, gid from username %q: %v",
+				username, err)
+		}
+		groups, err := UserGroupIds(username)
+		if err != nil {
+			return "", fmt.Errorf("get user group ids from username %q: %v",
+				username, err)
 		}
 		command.SysProcAttr = &syscall.SysProcAttr{}
 		command.SysProcAttr.Credential = credentials
+		command.SysProcAttr.Credential.Groups = groups
 	}
 
 	command.Env = os.Environ()
@@ -35,12 +40,12 @@ func ExecuteCommand(cmd string, username string, workDir string, args ...string)
 	command.Stderr = &errorBuf
 
 	if err := command.Start(); err != nil {
-		return "", fmt.Errorf("start command %q: %v: stderr: %s, stdout: %s",
+		return "", fmt.Errorf("start command %v: %v: stderr: %s, stdout: %s",
 			cmd, err, errorBuf.String(), outBuf.String())
 	}
 
 	if err := command.Wait(); err != nil {
-		return "", fmt.Errorf("wait for command %q: %v: stderr: %s, stdout: %s",
+		return "", fmt.Errorf("wait for command %v: %v: stderr: %s, stdout: %s",
 			cmd, err, errorBuf.String(), outBuf.String())
 	}
 
@@ -65,7 +70,7 @@ func UserCredentialsFromUsername(username string) (*syscall.Credential, error) {
 func UIDFromUsername(username string) (uint32, error) {
 	user, err := user.Lookup(username)
 	if err != nil {
-		return 0, fmt.Errorf("lookup user %s: %v", username, err)
+		return 0, fmt.Errorf("lookup user %q: %v", username, err)
 	}
 	uid, err := strconv.ParseUint(user.Uid, 10, 32)
 	if err != nil {
@@ -78,13 +83,35 @@ func UIDFromUsername(username string) (uint32, error) {
 func GIDFromUsername(username string) (uint32, error) {
 	user, err := user.Lookup(username)
 	if err != nil {
-		return 0, fmt.Errorf("lookup user %s: %v", username, err)
+		return 0, fmt.Errorf("lookup user %q: %v", username, err)
 	}
 	gid, err := strconv.ParseUint(user.Gid, 10, 32)
 	if err != nil {
 		return 0, fmt.Errorf("parse gid from string: %v", err)
 	}
 	return uint32(gid), nil
+}
+
+// UserGroupIds returns the ids of the groups that a user
+// belongs to.
+func UserGroupIds(username string) ([]uint32, error) {
+	groupIds := []uint32{}
+	user, err := user.Lookup(username)
+	if err != nil {
+		return []uint32{}, fmt.Errorf("lookup user %q: %v", username, err)
+	}
+	groups, err := user.GroupIds()
+	if err != nil {
+		return []uint32{}, fmt.Errorf("fetching user %q groups: %v", username, err)
+	}
+	for _, g := range groups {
+		gid, err := strconv.ParseUint(g, 10, 32)
+		if err != nil {
+			continue
+		}
+		groupIds = append(groupIds, uint32(gid))
+	}
+	return groupIds, nil
 }
 
 // Sha1Hmac returns the hmac sha1 hash of message m based on secret s.
